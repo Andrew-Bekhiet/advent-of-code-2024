868601edc9f5d6bd9202753568834155ef4ac514
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-12-01 22:40:26 +0200

    Day 1: using function matching and standard lib

--- Day-1/elixir.exs
+++ Day-1/elixir.exs
@@ -1,30 +1,35 @@
 defmodule Day1 do
+  defp p1_pairwise([], []), do: 0
+
+  defp p1_pairwise([n1 | rest1], [n2 | rest2]) do
+    abs(n1 - n2) + p1_pairwise(rest1, rest2)
+  end
+
   def part1(use_example) do
     {locations1, locations2} = parse_input(use_example)
 
     sorted1 = locations1 |> Enum.sort()
     sorted2 = locations2 |> Enum.sort()
 
-    Enum.zip(sorted1, sorted2)
-    |> Enum.map(fn {n1, n2} -> abs(n1 - n2) end)
-    |> Enum.sum()
+    p1_pairwise(sorted1, sorted2)
+  end
+
+  defp p2_similarity([], _), do: 0
+
+  defp p2_similarity([n | list], similarity_map) do
+    n * Map.get(similarity_map, n, 0) + p2_similarity(list, similarity_map)
+  end
+
+  defp p2_similarity([n | list], %{} = similarity_map) do
+    p2_similarity(list, similarity_map)
   end
 
   def part2(use_example) do
     {locations, list2} = parse_input(use_example)
 
-    similarity_map =
-      list2
-      |> Enum.reduce(%{}, fn n, acc ->
-        acc
-        |> Map.update(n, 1, &(&1 + 1))
-      end)
+    similarity_map = list2 |> Enum.frequencies()
 
-    IO.puts(inspect(similarity_map))
-
-    locations
-    |> Enum.map(fn n1 -> n1 * (similarity_map |> Access.get(n1, 0)) end)
-    |> Enum.sum()
+    p2_similarity(locations, similarity_map)
   end
 
   @spec parse_input(boolean) :: list({String.t(), list(pos_integer())})
@@ -35,10 +40,8 @@ defmodule Day1 do
     |> File.read!()
     |> String.split("\n", trim: true)
     |> Enum.reduce({[], []}, fn line, {list1, list2} ->
-      [s1, s2] = String.split(line, "   ", parts: 2)
-
-      {num1, _} = Integer.parse(s1)
-      {num2, _} = Integer.parse(s2)
+      {num1, rest} = Integer.parse(line)
+      {num2, _} = rest |> String.trim() |> Integer.parse()
 
       {[num1 | list1], [num2 | list2]}
     end)
